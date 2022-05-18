db33b4d3b71702883c2bf46a9bbd4342c459abc4
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-05-18 15:36:28 -0300

    build the interviewer list

---
+++ src/components/InterviewerList.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import "components/InterviewerList.scss";
+import InterviewerListItem from './InterviewerListItem';
+
+export default function InterviewerList(props) {
+  const interviewers = props.interviewers.map((interviewer) => {
+    console.log(interviewer)
+    return <InterviewerListItem
+      key = {interviewer.id}
+      name = {interviewer.name}
+      avatar = {interviewer.avatar}
+      //passing the function down through the comp.
+      setInterviewer = {() => props.setInterviewer(interviewer.id)}
+      interviewer = {props.interviewer}   
+      //if the persons id matches select it
+      selected = {interviewer.id === props.interviewer}
+    />
+  })
+  return (
+    <section className="interviewers">
+      <h4 className="interviewers__header text--light">interviewer</h4>
+      <ul className="interviewers__list">{interviewers}</ul>
+    </section>
+  );
+}
