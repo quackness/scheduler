8f98473015c905f6afad0b61b00d1919150b4699
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-05-19 12:19:25 -0300

    add story for Empty component to handle onClick and refactor Empty component to display props it is passed with

--- stories/index.js
+++ stories/index.js
@@ -11,7 +11,8 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
-import Header from "components/Appointment/Header.js";
+import Header from "components/Appointment/Header";
+import Empty from "components/Appointment/Empty";
 
 
 storiesOf("Button", module)
@@ -160,7 +161,8 @@ storiesOf("InterviewerList", module)
   //that returns a React component.
     .add("Appointment", () => <Appointment />)
     .add("Appointment with Time", () => <Appointment time="12pm" />)
-    .add("Header", () => <Header time="12pm" />);
+    .add("Header", () => <Header time="12pm" />)
+    .add("Empty", () => <Empty onAdd={action("onAdd")} />)
 
 
 
