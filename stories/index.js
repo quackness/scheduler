b64eb29523afbd60429642ad7cc910b777f8f629
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-05-19 11:59:19 -0300

    refactor header with props.time

--- stories/index.js
+++ stories/index.js
@@ -11,7 +11,7 @@ import DayList from "components/DayList";
 import InterviewerListItem from "components/InterviewerListItem";
 import InterviewerList from "components/InterviewerList";
 import Appointment from "components/Appointment/index.js";
-import Header from "components/Appointment/index.js";
+import Header from "components/Appointment/Header.js";
 
 
 storiesOf("Button", module)
