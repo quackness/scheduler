1f4c4d7c5a656d5bdfb6ea8b26da1c344eed00cb
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-05-20 14:18:10 -0300

    add story for appointment empty and conditional rendering

--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Fragment } from "react";
 
 import { storiesOf } from "@storybook/react";
 import { action } from "@storybook/addon-actions";
@@ -176,7 +176,9 @@ storiesOf("InterviewerList", module)
     .add("Error", () => <Error message="Could not delete appointment." onClose={action("onClose")} />)
     .add("Edit", () => <Form student="Karolina Redden" interviewer={1} interviewers={interviewers} 
     onSave={action("onSave")} onCancel={action("onCancel")}/>)
-    .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>);
+    .add("Create", () => <Form interviewers={interviewers} onSave={action("onSave")} onCancel={action("onCancel")}/>)
+    .add("Appointment Empty", () => (<Fragment> <Appointment id={1} time="4pm" />
+    <Appointment time="5pm" /> </Fragment>))
     
 
 
