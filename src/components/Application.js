508f4cb6f3dcec778ecb712cb3eac0b2a190a3ff
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-05-20 15:31:16 -0300

    add schedule

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import "components/Application.scss";
 import DayList from "components/DayList";
+import Appointment from "components/Appointment";
 
 
 //array moved from stories
@@ -22,9 +23,60 @@ const days = [
   },
 ];
 
+const appointments = {
+  "1": {
+    id: 1,
+    time: "12pm",
+  },
+  "2": {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer:{
+        id: 3,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  "3": {
+    id: 3,
+    time: "2pm",
+  },
+  "4": {
+    id: 4,
+    time: "3pm",
+    interview: {
+      student: "Archie Andrews",
+      interviewer:{
+        id: 4,
+        name: "Cohana Roy",
+        avatar: "https://i.imgur.com/FK8V841.jpg",
+      }
+    }
+  },
+  "5": {
+    id: 5,
+    time: "4pm",
+  }
+};
+
+
 
 export default function Application(props) {
   const [day, setDay] = useState('Monday');
+
+  const appointmentComponents = Object.values(appointments).map((appointment) => {
+    console.log(appointment);
+    return <Appointment
+    key={appointment.id} 
+    // spread the object into the props definition
+    {...appointment} 
+  />
+  }) 
+  
+
   return (
     <main className="layout">
       <section className="sidebar">
@@ -56,7 +108,8 @@ export default function Application(props) {
         />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointmentComponents}
+      <Appointment key="last" time="5pm" />
       </section>
     </main>
   );
