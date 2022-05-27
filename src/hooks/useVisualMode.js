aaf271f900e807b174bfa7ca320d872dccf68863
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-05-26 23:39:24 -0300

    add function back()

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -6,23 +6,47 @@ import React, { useState } from "react";
 // return an object with a mode property
 export default function useVisualMode(initial) {
   const [mode, setMode] = useState(initial)
-  console.log("mode is", mode)
+  console.log("mode is", mode);
+  //history of the modes, so we can go backwards
+  const [history, setHistory] = useState([initial]);
 
-  const transition = function (version) {//first {{}
+  function transition(version) {//first {{}
   //  change the mode with setMode function,   
    setMode(version);
+   //When transition is called, we need to add the new mode to our history.
+   setHistory( prev => [...prev, version]);
+
   }
+  function back() {
+    //push to history?
+    //sets the 
+    //[one, two, three]
+//remove history -1
+//[one, two]
+    setHistory( prev => [ ...prev.slice(0, -1)]);
+    //[one, two, three] - 2 becouse setHistory is async
+    setMode(history[history.length - 2]);
+   
+    // setHistory.push(newMode)
+    // toPop.pop(setHistory -1)
+    
+  }
+  console.log("back", back)
   //return the object containing the initial version and the version from the transition function
   //call with transition
 
-  return {mode, transition}
+  return {mode, transition, back}
 
 };
+//: newMode[newMode.length - 1]
+// hint
+// function useCustomHook() {
+//   function action() {}
+//   return { action };
+// }
 
 
-//arr length is 2, when we do -1 we go 2-1 = position 1, which is the second
-//let mode = ["first", "second"];
 
-
-
-
+// take in an initial mode
+// set the mode state with the initial mode provided
+// return an object with a mode property
