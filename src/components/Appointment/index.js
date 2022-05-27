fabb68e79f2664b0aa7ad24c85bd2dc81916d52c
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-05-27 11:56:13 -0300

    add Create constant and update onAdd function in Empty to transition to the CREATE mode when the user clicks the add appointment button

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -8,27 +8,29 @@ import useVisualMode from "hooks/useVisualMode";
 
 
 export default function Appointment(props) {
-
   const EMPTY = "EMPTY";
   const SHOW = "SHOW";
-
-
+  const CREATE = "CREATE";
+//add the custom hook
   const {mode, transition, back} = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
-
-
   return (
     <article className="appointment">
-      <Header time={props.time} />
-      {props.interview ?
-        <Show 
-        student = {props.interview.student}
-        interviewer={props.interview.interviewer.name}
-        />
-        :
-        <Empty />
-      }
+        <Header time={props.time} />
+          {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+          {mode === SHOW && (
+            <Show
+              student={props.interview.student}
+              interviewer={props.interview.interviewer}
+              // onEdit={props.onEdit}
+              // onDelete={props.onDelete}
+
+            />
+          )}
     </article>
   );
 }
+
+{/* <Show student="Lydia Miller-Jones" interviewer={interviewers[0].name} 
+    onEdit={action("onEdit")} onDelete={action("onDelete")} />) */}
