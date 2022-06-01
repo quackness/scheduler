b7593167e2259edd965c82d15050e2ef5f9eaa9c
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-06-01 10:35:03 -0300

    create a file and add function

---
+++ src/hooks/useApplicationData.js
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+
+// Our useApplicationData Hook will return an object with four keys:
+
+//     The state object will maintain the same structure.
+//     The setDay action can be used to set the current day.
+//     The bookInterview action makes an HTTP request and updates the local state.
+//     The cancelInterview action makes an HTTP request and updates the local state.
+export default function useApplicationData(data) {
+
+}
