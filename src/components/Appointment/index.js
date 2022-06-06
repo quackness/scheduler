093c42aaba9ecbd44570fbb0cadf65a1d74d5389
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-06-06 15:13:12 -0300

    edit comments

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -20,11 +20,11 @@ export default function Appointment(props) {
   const DELETING = "DELETING";
   const CONFIRM = "CONFIRM";
   const EDIT = "EDIT";
-  const ERROR_SAVE  = "ERROR_SAVE";
+  const ERROR_SAVE = "ERROR_SAVE";
   const ERROR_DELETE = "ERROR_DELETE";
 
-//add the custom hook
-  const {mode, transition, back} = useVisualMode(
+  //add the custom hook
+  const { mode, transition, back } = useVisualMode(
     props.interview ? SHOW : EMPTY
   );
 
@@ -39,16 +39,16 @@ export default function Appointment(props) {
     //console.log("props.bookInterview", props.bookInterview(props.id, interview))
     transition(SAVING);
     props.bookInterview(props.id, interview)
-    .then(() => //interview is from the function save
-    transition(SHOW))//could this be done within the function pointers?
-    .catch(() => transition(ERROR_SAVE, true));
+      .then(() => //interview is from the function save
+        transition(SHOW))//could this be done within the function pointers?
+      .catch(() => transition(ERROR_SAVE, true));
   }
 
   function removeInterview() {
     transition(DELETING, true)
     props.cancelInterview(props.id)
-    .then(() => transition(EMPTY))
-    .catch(() => transition(ERROR_DELETE, true))
+      .then(() => transition(EMPTY))
+      .catch(() => transition(ERROR_DELETE, true))
   }
 
   //when we need to change the interiview
@@ -58,55 +58,55 @@ export default function Appointment(props) {
 
   return (
     <article className="appointment">
-        <Header time={props.time} />
-          {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
-          {mode === SHOW && (
-            <Show
-              student={props.interview.student}
-              interviewer={props.interview.interviewer}
-              onEdit={editInterview}
-              //instead of deleting it will go to the CONFIRM
-              onDelete={() => transition(CONFIRM)}
-              />
-          )}
-          {mode === CREATE && (<Form 
-          
-          interviewers = {props.interviewers}
-          onCancel = {back}
-          onSave = {save}
-          />)}
-
-          {mode === SAVING && <Status
-          message={"Saving"}
-          />}
-
-          {mode === DELETING && <Status 
-          message={"Deleting appointment"}
-          />}
-
-          {mode === CONFIRM && <Confirm
-          message={"Are you sure you want to delete this interview?"}
-          onConfirm={removeInterview}
-          onCancel={back}//back is coming from a custom hook
-          />}
-
-          {mode === EDIT && (<Form 
+      <Header time={props.time} />
+      {mode === EMPTY && <Empty onAdd={() => transition(CREATE)} />}
+      {mode === SHOW && (
+        <Show
           student={props.interview.student}
-          interviewers = {props.interviewers}
-          onCancel = {() => {back()}}
-          onSave = {save}
-          />)}
+          interviewer={props.interview.interviewer}
+          onEdit={editInterview}
+          //instead of deleting it will go to the CONFIRM
+          onDelete={() => transition(CONFIRM)}
+        />
+      )}
+      {mode === CREATE && (<Form
 
-          {mode === ERROR_DELETE && <Error
-          message={"Cannot cancel appointment"}
-          onClose={back}
-          />}
+        interviewers={props.interviewers}
+        onCancel={back}
+        onSave={save}
+      />)}
 
-        {mode === ERROR_SAVE && <Error
-          message={"Cannot save appointment"}
-          onClose={back}
-          />}
-         
+      {mode === SAVING && <Status
+        message={"Saving"}
+      />}
+
+      {mode === DELETING && <Status
+        message={"Deleting appointment"}
+      />}
+
+      {mode === CONFIRM && <Confirm
+        message={"Are you sure you want to delete this interview?"}
+        onConfirm={removeInterview}
+        onCancel={back}//back is coming from a custom hook
+      />}
+
+      {mode === EDIT && (<Form
+        student={props.interview.student}
+        interviewers={props.interviewers}
+        onCancel={() => { back() }}
+        onSave={save}
+      />)}
+
+      {mode === ERROR_DELETE && <Error
+        message={"Cannot cancel appointment"}
+        onClose={back}
+      />}
+
+      {mode === ERROR_SAVE && <Error
+        message={"Cannot save appointment"}
+        onClose={back}
+      />}
+
     </article>
   );
 }
