9e6cff327d5d207c57f94675b9d771255ec7a68e
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-06-07 12:37:55 -0300

    add imports, cleanup and describe

---
+++ src/components/__tests__/Form.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { render, cleanup } from "@testing-library/react";
+import Form from "components/Appointment/Form";
+
+afterEach(cleanup);
+describe("Form", () => {
+
+});
