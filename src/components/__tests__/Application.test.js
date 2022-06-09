8cfb38428dc7c0f3b56f8f3fb445fdd5c49a557d
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-06-09 11:32:49 -0300

    add test for changes the schedule when a new day is selected with a promise and async

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,18 +6,32 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
+xit("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
   return waitForElement(() => getByText('Monday'));
 });
 
-
+//promise
 it("defaults to Monday and changes the schedule when a new day is selected", () => {
   const { getByText } = render(<Application />);
 
   return waitForElement(() => getByText("Monday")).then(() => {
     fireEvent.click(getByText("Tuesday"));
+
     expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 });
+
+//the same test but with asyn/await which hides the promise
+it("changes the schedule when a new day is selected", async () => {
+  const { getByText } = render(<Application />);
+
+  await waitForElement(() => getByText("Monday"));
+
+  fireEvent.click(getByText("Tuesday"));
+
+  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+});
+
+
