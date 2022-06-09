b7dcea6cc66cf48de4960871ad3537584c14e41a
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-06-09 11:38:08 -0300

    add describe

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -6,24 +6,24 @@ import Application from "components/Application";
 
 afterEach(cleanup);
 
-xit("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+// xit("defaults to Monday and changes the schedule when a new day is selected", () => {
+//   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText('Monday'));
-});
+//   return waitForElement(() => getByText('Monday'));
+// });
 
 //promise
-it("defaults to Monday and changes the schedule when a new day is selected", () => {
-  const { getByText } = render(<Application />);
+// it("defaults to Monday and changes the schedule when a new day is selected", () => {
+//   const { getByText } = render(<Application />);
 
-  return waitForElement(() => getByText("Monday")).then(() => {
-    fireEvent.click(getByText("Tuesday"));
+//   return waitForElement(() => getByText("Monday")).then(() => {
+//     fireEvent.click(getByText("Tuesday"));
 
-    expect(getByText("Leopold Silvers")).toBeInTheDocument();
-  });
-});
+//     expect(getByText("Leopold Silvers")).toBeInTheDocument();
+//   });
+// });
 
-//the same test but with asyn/await which hides the promise
+describe("Application", () => {
 it("changes the schedule when a new day is selected", async () => {
   const { getByText } = render(<Application />);
 
@@ -32,6 +32,5 @@ it("changes the schedule when a new day is selected", async () => {
   fireEvent.click(getByText("Tuesday"));
 
   expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  });
 });
-
-
