d8ef060fdb5565204f2fda7af0c274a1ac615890
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-06-10 14:23:45 -0300

    add test and implement logic for loads data, edits an interview and keeps the spots remaining for Monday the same

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -53,7 +53,7 @@ describe("Application", () => {
 
     expect(
       getByText(appointment, "Are you sure you want to delete this interview?")
-      ).toBeInTheDocument();
+    ).toBeInTheDocument();
 
     fireEvent.click(getByText(appointment, "Confirm"));
 
@@ -63,18 +63,43 @@ describe("Application", () => {
 
     const day = getAllByTestId(container, "day").find(day =>
       queryByText(day, "Monday")
-      );
+    );
 
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   })
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+
+    const { container, debug } = render(<Application />);
+    await waitForElement(() => getByText(container, "Archie Cohen"));
+
+    const appointment = getAllByTestId(container, "appointment").find(
+      appointment => queryByText(appointment, "Archie Cohen")
+    );
+    fireEvent.click(getByAltText(appointment, "Edit"));
+
+    // fireEvent.change(getAllByTestId(appointment, "student-name-input"), {
+    //   target: { value: "Lydia Miller-Jones" }
+    // });
+
+    fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" }
+    });
+
+    fireEvent.click(getByText(appointment, "Save"));
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+    fireEvent.click(getByText(appointment, "Save"));
+
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday")
+    );
+
+    expect(getByText(day, "1 spot remaining")).toBeInTheDocument();
+    debug();
+  })
 });
 
-
-
-
-
-
-
+ 
 
 
 
