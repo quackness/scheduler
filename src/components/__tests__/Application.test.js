a4f301673ef111021b4148eaf8ba9ae14b454871
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-06-09 21:46:23 -0300

    add tests: wait until the element with the text with the name  is displayed and check that Monday has text no spots remaining

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText } from "@testing-library/react";
+import { render, cleanup, waitForElement, fireEvent, getByText, prettyDOM, getAllByTestId, getByAltText, getByPlaceholderText, queryByText } from "@testing-library/react";
 
 import Application from "components/Application";
 
@@ -24,41 +24,47 @@ afterEach(cleanup);
 // });
 
 describe("Application", () => {
-it("changes the schedule when a new day is selected", async () => {
-  const { getByText } = render(<Application />);
-  await waitForElement(() => getByText("Monday"));
-  fireEvent.click(getByText("Tuesday"));
-  expect(getByText("Leopold Silvers")).toBeInTheDocument();
+  it("changes the schedule when a new day is selected", async () => {
+    const { getByText } = render(<Application />);
+    await waitForElement(() => getByText("Monday"));
+    fireEvent.click(getByText("Tuesday"));
+    expect(getByText("Leopold Silvers")).toBeInTheDocument();
   });
 
   //ByLabelText, ByPlaceholderText, ByText, ByDisplayValue, ByAltText, ByTitle and ByRole
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container, debug } = render (<Application />);
+    const { container, debug } = render(<Application />);
     //console.log("container>>>", container)
 
     await waitForElement(() => getByText(container, "Archie Cohen"));
     console.log("prettyDOM container", prettyDOM(container));
 
     const appointment = getAllByTestId(container, "appointment")[0];
-    
+
 
     fireEvent.click(getByAltText(appointment, "Add"));
 
     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
-      target: {value: "Lydia Miller-Jones"}
+      target: { value: "Lydia Miller-Jones" }
     });
 
     fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
-    
+
     fireEvent.click(getByText(appointment, "Save"));
     expect(getByText(appointment, "Saving")).toBeInTheDocument();
 
     console.log("prettyDOM appointment", prettyDOM(appointment));
+    console.log("debug", prettyDOM(debug));
 
+    await waitForElement(() => getByText(appointment, "Lydia Miller-Jones"));
 
+    const day = getAllByTestId(container, "day").find(day =>
+      queryByText(day, "Monday"));
+    expect(getByText(day, "no spots remaining")).toBeInTheDocument();
+
+    console.log("prettyDOm day", prettyDOM(day));
 
   });
-
 });
 
 
@@ -69,6 +75,6 @@ it("changes the schedule when a new day is selected", async () => {
 // Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name".done
 // Click the first interviewer in the list.done
 // Click the "Save" button on that same appointment.done
-// Check that the element with the text "Saving" is displayed.
+// Check that the element with the text "Saving" is displayed.done
 // Wait until the element with the text "Lydia Miller-Jones" is displayed.
 // Check that the DayListItem with the text "Monday" also has the text "no spots remaining".
