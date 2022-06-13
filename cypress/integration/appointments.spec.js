2a8f7a06146d7b5464a500a2b95d1cbf818a9d6b
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-06-13 15:51:28 -0300

    add test to click in the add button, type name and pick the interviewer

--- cypress/integration/appointments.spec.js
+++ cypress/integration/appointments.spec.js
@@ -4,7 +4,10 @@ describe("Appointments", () => {
     cy.contains("Monday")
     cy.get("[alt=Add]")
       .first()
-      .click();
+      .click()
+    cy.get("[data-testid=student-name-input]").type('Lydia Miller-Jones')
+    cy.get("[alt='Sylvia Palmer']").click();
+   
 
   })
 
