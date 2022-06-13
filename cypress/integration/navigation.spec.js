35b8d5534edc2e28930e5c421f3170820d21ad6a
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-06-13 13:45:42 -0300

    refactor test to check for the class name

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,11 +5,9 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    // cy.get('li')
-      cy.contains('li', 'Tuesday')
+      cy.contains("[data-testid=day]", 'Tuesday')
       .click()
-      // .should("have.css", "background-color", "rgb(242, 242, 242)");
-      .should("have.css", "background-color", "rgb(242, 242, 242)")
+      .should("have.class", "day-list__item--selected")
 
   })
 });
