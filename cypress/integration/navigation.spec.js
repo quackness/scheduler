292f4c00e5d4b14ccc3f50acc2aac8a18911aedb
Karolina <karolinadubaj@Karolinas-MBP.localdomain>
2022-06-13 13:37:16 -0300

    refactor test

--- cypress/integration/navigation.spec.js
+++ cypress/integration/navigation.spec.js
@@ -5,7 +5,11 @@ describe("Navigation", () => {
 
   it("should navigate to Tuesday", () => {
     cy.visit("/");
-    cy.get('li').contains('Tuesday').click();
+    // cy.get('li')
+      cy.contains('li', 'Tuesday')
+      .click()
+      // .should("have.css", "background-color", "rgb(242, 242, 242)");
+      .should("have.css", "background-color", "rgb(242, 242, 242)")
 
   })
 });
